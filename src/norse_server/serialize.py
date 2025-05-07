c40001594a18f6433b82b19891b32d4560bfb651
Sebastian Spreizer <spreizer@web.de>
2025-05-07 09:11:22 +0200

    Add more serialization options

--- src/norse_server/serialize.py
+++ src/norse_server/serialize.py
@@ -10,11 +10,13 @@ from .exceptions import call_or_error
 @call_or_error
 def serialize_data(data):
     if isinstance(data, torch.Tensor):
-        return data.detach().tolist()
+        return serialize_data(data.detach().tolist())
     elif isinstance(data, np.ndarray):
-        return data.tolist()
+        return serialize_data(ata.tolist())
+    elif isinstance(data, np.integer):
+        return int(data)
     elif isinstance(data, dict):
-        return {k: serialize_data(v) for k, v in data.items()}
-    elif isinstance(data, list):
+        return dict([(serialize_data(k), serialize_data(v)) for k, v in data.items()])
+    elif isinstance(data, (list, tuple)):
         return [serialize_data(v) for v in data]
     return data
