305805e76a3a0f3aaf671c59bd1edd454c89c3f2
Sebastian Spreizer <spreizer@web.de>
2025-05-07 09:12:23 +0200

    Remove flask from logger

--- src/norse_server/logger.py
+++ src/norse_server/logger.py
@@ -3,8 +3,6 @@
 
 import os # noqa
 import logging # noqa
-from flask.logging import default_handler # noqa
 
 logger = logging.getLogger()
-logger.addHandler(default_handler)
 logger.setLevel(os.getenv("NORSE_SERVER_LOGLEVEL", "INFO"))
